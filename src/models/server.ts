67e0a71da1a175bf901ddcbc560575c17cd9ae19
PressureDraper <warmarines00@hotmail.com>
2024-12-13 20:59:41 -0600

    add: rest service for getting last 13 tickets

--- src/models/server.ts
+++ src/models/server.ts
@@ -3,12 +3,14 @@ import { Server as HttpServer } from 'http';
 import { Socket } from 'socket.io';
 import Sockets from './sockets';
 import cors from 'cors';
+import Ticket from './ticket';
 
 class Server {
     private app: Express;
     private port: string;
     private server: HttpServer;
     private io: Socket;
+    private sockets: Sockets;
 
     constructor() {
 
@@ -25,26 +27,31 @@ class Server {
                 methods: ['GET', 'POST']
             }
         });
+
+        //initialize sockets
+        this.sockets = new Sockets(this.io);
     }
 
     middlewares() {
         this.app.use(express.static(__dirname + '../../../public'));
-
         this.app.use(cors({ //only enable cors if clients will connect from remote domains or ports besides 'origin'
             origin: '*', // all domains allowed use *
             methods: ['GET', 'POST'],
             allowedHeaders: ['Content-Type', 'Authorization']
         }));
-    }
 
-    socketIOConfig() {
-        new Sockets(this.io);
+        this.app.use('/history', (req, res) => {
+            const ticketList: Ticket[] = this.sockets.ticketList.last13Tickets;
+
+            res.json({
+                ok: true,
+                data: ticketList
+            });
+        });
     }
 
     execute() {
-
         this.middlewares();
-        this.socketIOConfig();
 
         this.server.listen(this.port, () => {
             console.log(`Server Running on port ${this.port}...`);
